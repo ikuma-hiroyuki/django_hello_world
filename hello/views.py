684766433c1137240efe705366cf6d33086294a7
ikuma <chemicalbondo@gmail.com>
2023-01-13 14:17:23 +0900

    HelloViewクラスとhello.htmlの追加

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
 def hello(request):
     return HttpResponse('<h1>hello world</h1>')
+
+
+class HelloView(TemplateView):
+    template_name = 'hello.html'
