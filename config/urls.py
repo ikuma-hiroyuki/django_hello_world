17e68b95272fe8b9693a21a5dc64aa6989e6a32c
ikuma <chemicalbondo@gmail.com>
2023-01-13 14:16:59 +0900

    hello2の追加

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from hello import views
+
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
     path('hello/', include('hello.urls')),
+    path('hello2/', views.HelloView.as_view(), name='hello_class'),
 ]
